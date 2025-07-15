b93ee877aee7e133b05deb554bdfa792bcd1b7fd
tunderx59122 <thomasipod59122@gmail.com>
2025-07-15 20:29:48 +0200

    add frontend part of middleware to separate from it

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ import commentRoutes from './routes/comments';
 import notificationRoutes from './routes/notifications';
 import profileRoutes from './routes/userRoutes/profile';
 import followRoutes from './routes/userRoutes/follow';
+import requireAuth from './middleware/requireAuth';
 
 const app: Application = express();
 
@@ -66,7 +67,7 @@ if (NODE_ENV === 'development') {
 app.use(express.json({ limit: '16mb' }));
 app.use(morgan('dev'));
 app.use(cookieParser());
-
+app.use(requireAuth)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 
